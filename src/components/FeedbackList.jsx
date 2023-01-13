b4563adb5bbccd38c6d9beb51babb9b1147c0752
Samuel <samuel.cflau@gmail.com>
2023-01-14 02:37:52 +0800

    16. <Card> Component & Conditional Styles 10 分鐘 (Typechecking With PropTypes)

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import FeedbackItem from './FeedbackItem'
+import React from "react";
+import PropTypes from "prop-types";
+import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({feedbacks}) {
-    if(!feedbacks || feedbacks.length === 0) {
-        return <p>No Feedback Yet</p>
-    }
+function FeedbackList({ feedbacks }) {
+  if (!feedbacks || feedbacks.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
   return (
-    <div className='feedback-list'>
-        {feedbacks.map(item => (
-            <FeedbackItem key={item.id} item={item}/>
-        ))}
+    <div className="feedback-list">
+      {feedbacks.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default FeedbackList
+FeedbackList.propTypes = {
+  feedbacks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default FeedbackList;
