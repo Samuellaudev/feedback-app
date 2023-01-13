d1cecf0e4ce2151a578971ed205a9e857a184ba7
Samuel <samuel.cflau@gmail.com>
2023-01-14 01:11:07 +0800

    14. State & useState Hook 8 分鐘 (Component-level state)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem'
 
 function App() {
   return (
     <>
         <Header />
+        <FeedbackItem />
     </>
   )
 }
