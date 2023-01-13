fa9e006f3ffef83c4f455eba8a36209a94861816
Samuel <samuel.cflau@gmail.com>
2023-01-14 00:47:24 +0800

    12. Creating Your First Component & Props 8 分鐘 (rfce; defaultProps; propTypes)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
+import Header from './components/Header'
+
 function App() {
-  const title = "Blog Post";
-  const body = "This is a blog post";
-  const comments = [
-    { id: 1, text: "Comment one" },
-    { id: 2, text: "Comment two" },
-    { id: 3, text: "Comment three" },
-  ];
-
-  const loading = false;
-  const showComments = false;
-
-  const commentBlock = (
-    <div className="comments">
-      <h3>Comments {comments.length}</h3>
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment.id}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
-  if (loading) return <h1>Loading...</h1>;
-
   return (
-    <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      {showComments && commentBlock}
-    </div>
-  );
+    <>
+        <Header text='Hello World'/>
+    </>
+  )
 }
 
 export default App;
