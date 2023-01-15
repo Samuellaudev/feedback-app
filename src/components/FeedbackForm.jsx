2118a065a4c499d3e1ea255567b2a9917de164df
Samuel <samuel.cflau@gmail.com>
2023-01-15 17:06:02 +0800

    21. Real-Time Validation 5 分鐘 (setBtndisabled; setMessage)

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,7 +5,21 @@ import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnIsDisabled, setBtnIsDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
   const handleChange = (e) => {
+    if (text === "") {
+      setBtnIsDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length < 9) {
+      setBtnIsDisabled(true);
+      setMessage("Text must be greater than 10 characters");
+    } else {
+      setBtnIsDisabled(false);
+      setMessage(null);
+    }
+
     setText(e.target.value);
   };
   return (
@@ -15,11 +29,18 @@ function FeedbackForm() {
         {/* todo - rating select component */}
         <div className="input-group">
           <input onChange={handleChange} type="text" value={text} />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnIsDisabled}>
+            Send
+          </Button>
         </div>
       </form>
+      {message && <div className="message">{message}</div>}
     </Card>
   );
 }
+
+FeedbackForm.propTypes = {
+  text: PropTypes.string,
+};
 
 export default FeedbackForm;
