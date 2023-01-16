b80dcd1dabc8d76913258504ad51d91573f28ee2
Samuel <samuel.cflau@gmail.com>
2023-01-17 00:54:41 +0800

    25. Creating Routes (React Router 6)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
+import AboutPage from "../src/pages/AboutPage";
 
 function App() {
-  const [feedbacks, setFeedbacks] = useState(FeedbackData);
+	const [feedbacks, setFeedbacks] = useState(FeedbackData);
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedbacks([newFeedback, ...feedbacks]);
-  };
+	const addFeedback = (newFeedback) => {
+		newFeedback.id = uuidv4();
+		setFeedbacks([newFeedback, ...feedbacks]);
+	};
 
-  const deleteFeedback = (id) => {
-    if (window.confirm(`Are you sure you want to delete id:${id}?`)) {
-      setFeedbacks(feedbacks.filter((item) => item.id !== id));
-    }
-  };
-  return (
-    <>
-      <Header />
-      <FeedbackForm handleAdd={addFeedback} />
-      <FeedbackStats feedbacks={feedbacks} />
-      <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
-    </>
-  );
+	const deleteFeedback = (id) => {
+		if (window.confirm(`Are you sure you want to delete id:${id}?`)) {
+			setFeedbacks(feedbacks.filter((item) => item.id !== id));
+		}
+	};
+	return (
+		<Router>
+			<Header />
+			<div className="container">
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							<>
+								<FeedbackForm handleAdd={addFeedback} />
+								<FeedbackStats feedbacks={feedbacks} />
+								<FeedbackList
+									feedbacks={feedbacks}
+									handleDelete={deleteFeedback}
+								/>
+							</>
+						}
+					></Route>
+					<Route path="/about" element={<AboutPage />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -4,43 +4,43 @@ import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList({ feedbacks, handleDelete }) {
-  if (!feedbacks || feedbacks.length === 0) {
-    return <p>No Feedback Yet</p>;
-  }
+	if (!feedbacks || feedbacks.length === 0) {
+		return <p>No Feedback Yet</p>;
+	}
 
-  //   With Framer-motion
-  return (
-    <div className="feedback-list">
-      <AnimatePresence>
-        {feedbacks.map((item) => {
-          return (
-            <motion.div
-              key={item.id}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-            >
-              <FeedbackItem
-                key={item.id}
-                item={item}
-                handleDelete={handleDelete}
-              />
-            </motion.div>
-          );
-        })}
-      </AnimatePresence>
-    </div>
-  );
+	//   With Framer-motion
+	return (
+		<div className="feedback-list">
+			<AnimatePresence>
+				{feedbacks.map((item) => {
+					return (
+						<motion.div
+							key={item.id}
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+						>
+							<FeedbackItem
+								key={item.id}
+								item={item}
+								handleDelete={handleDelete}
+							/>
+						</motion.div>
+					);
+				})}
+			</AnimatePresence>
+		</div>
+	);
 }
 
 FeedbackList.propTypes = {
-  feedbacks: PropTypes.arrayOf(
-    PropTypes.shape({
-      //   id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  ),
+	feedbacks: PropTypes.arrayOf(
+		PropTypes.shape({
+			//   id: PropTypes.number.isRequired,
+			text: PropTypes.string.isRequired,
+			rating: PropTypes.number.isRequired,
+		})
+	),
 };
 
 export default FeedbackList;
