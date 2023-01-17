74e92138cc9e8c4dc112db8122469e5ab8454410
Samuel <samuel.cflau@gmail.com>
2023-01-17 22:03:10 +0800

    feat: add header redirect-click

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,58 +5,58 @@ import Button from "./shared/Button";
 import RatingSelect from "./RatingSelect";
 
 function FeedbackForm({ handleAdd }) {
-  const [text, setText] = useState("");
-  const [rating, setRating] = useState(10);
-  const [btnIsDisabled, setBtnIsDisabled] = useState(true);
-  const [message, setMessage] = useState("");
+	const [text, setText] = useState("");
+	const [rating, setRating] = useState(10);
+	const [btnIsDisabled, setBtnIsDisabled] = useState(true);
+	const [message, setMessage] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (text.trim().length > 10) {
-      const newFeedback = {
-        text,
-        rating,
-      };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (text.trim().length > 10) {
+			const newFeedback = {
+				text,
+				rating,
+			};
 
-      handleAdd(newFeedback);
+			handleAdd(newFeedback);
 
-      setText("");
-    }
-  };
+			setText("");
+		}
+	};
 
-  const handleChange = (e) => {
-    if (text === "") {
-      setBtnIsDisabled(true);
-      setMessage(null);
-    } else if (text !== "" && text.trim().length < 9) {
-      setBtnIsDisabled(true);
-      setMessage("Text must be greater than 10 characters");
-    } else {
-      setBtnIsDisabled(false);
-      setMessage(null);
-    }
+	const handleChange = (e) => {
+		if (text === "") {
+			setBtnIsDisabled(true);
+			setMessage(null);
+		} else if (text !== "" && text.trim().length < 9) {
+			setBtnIsDisabled(true);
+			setMessage("Text must be greater than 10 characters");
+		} else {
+			setBtnIsDisabled(false);
+			setMessage(null);
+		}
 
-    setText(e.target.value);
-  };
-  return (
-    <Card>
-      <form onSubmit={handleSubmit}>
-        <h2>How would you rate your service with us?</h2>
-        <RatingSelect selectRating={(rating) => setRating(rating)} />
-        <div className="input-group">
-          <input onChange={handleChange} type="text" value={text} />
-          <Button type="submit" isDisabled={btnIsDisabled}>
-            Send
-          </Button>
-        </div>
-      </form>
-      {message && <div className="message">{message}</div>}
-    </Card>
-  );
+		setText(e.target.value);
+	};
+	return (
+		<Card>
+			<form onSubmit={handleSubmit}>
+				<h2>How would you rate your service with us?</h2>
+				<RatingSelect selectRating={(rating) => setRating(rating)} />
+				<div className="input-group">
+					<input onChange={handleChange} type="text" value={text} />
+					<Button type="submit" isDisabled={btnIsDisabled}>
+						Send
+					</Button>
+				</div>
+			</form>
+			{message && <div className="message">{message}</div>}
+		</Card>
+	);
 }
 
 FeedbackForm.propTypes = {
-  text: PropTypes.string,
+	text: PropTypes.string,
 };
 
 export default FeedbackForm;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,42 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
-function Header({text, bgColor, textColor}) { // destructuring
-    const headerStyles = {
-        backgroundColor: bgColor,
-        color: textColor
-    }
+function Header({ text, bgColor, textColor, cursorSymbol }) {
+	// destructuring
+	const headerStyles = {
+		backgroundColor: bgColor,
+		color: textColor,
+		cursor: cursorSymbol,
+	};
 
-  return (
-    <header style={headerStyles}>
-      <div className="container">
-        <h2>{text}</h2>
-      </div>
-    </header>
-  )
+	const navigate = useNavigate();
+
+	const redirect = () => {
+		console.log("hello");
+		navigate("/");
+	};
+
+	return (
+		<header style={headerStyles}>
+			<div className="container">
+				<h2 onClick={redirect}>{text}</h2>
+			</div>
+		</header>
+	);
 }
 
 Header.defaultProps = {
-  text: 'Feedback UI',
-  bgColor: 'rgba(0,0,0,0.4)',
-  textColor: '#ff6a95'
-}
+	text: "Feedback UI",
+	bgColor: "rgba(0,0,0,0.4)",
+	textColor: "#ff6a95",
+	cursorSymbol: "pointer",
+};
 
 Header.propTypes = {
-  text: PropTypes.string,
-  bgColor: PropTypes.string,
-  textColor: PropTypes.string
-}
+	text: PropTypes.string,
+	bgColor: PropTypes.string,
+	textColor: PropTypes.string,
+};
 
-export default Header
+export default Header;
