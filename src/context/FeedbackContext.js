5a23a136ee1608fabe72ae217c67a97d5539a4af
Samuel <samuel.cflau@gmail.com>
2023-01-18 22:04:56 +0800

    33. Edit Feedback Event 6 分鐘 (new global state: feedbackEdit; new function: editFeedback)

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -7,15 +7,30 @@ const FeedbackContext = createContext();
 export const FeedbackProvider = ({ children }) => {
 	const [feedbacks, setFeedbacks] = useState(FeedbackData);
 
+	const [feedbackEdit, setFeedbackEdit] = useState({
+		item: {}, // when the icon is clicked, the content about that item will be put in here
+		edit: false, // when the icon is clicked, it will change to 'true', which means we are in edit mode
+	});
+
+	// Add feedback
 	const addFeedback = (newFeedback) => {
 		newFeedback.id = uuidv4();
 		setFeedbacks([newFeedback, ...feedbacks]);
 	};
 
+	// Delete feedback
 	const deleteFeedback = (id) => {
 		if (window.confirm(`Are you sure you want to delete id:${id}?`)) {
 			setFeedbacks(feedbacks.filter((item) => item.id !== id));
 		}
+	};
+
+	// Set item to be updated
+	const editFeedback = (item) => {
+		setFeedbackEdit({
+			item,
+			edit: true,
+		});
 	};
 
 	return (
@@ -24,6 +39,7 @@ export const FeedbackProvider = ({ children }) => {
 				feedbacks,
 				addFeedback,
 				deleteFeedback,
+				editFeedback,
 			}}
 		>
 			{children}
